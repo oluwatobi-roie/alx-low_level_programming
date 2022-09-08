c3067149264a452609934d432c533bac684a5613
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-09-08 16:39:34 -0700

    Updated found bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,9 @@
  *
  * Return: Always 0 (sucessints)
  */
-
 #include <stdio.h>
-
-int main (void)
-{ 
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-
+	return (0);
 }

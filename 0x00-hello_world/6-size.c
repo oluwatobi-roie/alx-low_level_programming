c00f1d5f3c6e4fd7bc1fe010c546b960979f3fc7
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-09-09 00:01:11 -0700

    using d instead of others for print

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,27 +2,12 @@
  *here is the answer
 */
 #include <stdio.h>
-/*
- * main - 		Entry point of our applicatio
- * Description: 	This program prints out the size of each variable type in c programming
- * Return: 		Alwaus 0.
- */
 int main(void)
-	/*
-	 * main - 		Entry point
-	 * Description: 	'This print out some letters'
-	 * Return: 		alway 0.
-	 */
 {
-	/**
-	 * main - 		Entry function
-	 * Description: 	'This print out some letters'
-	 * Return: 		always 0
-	 */
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }

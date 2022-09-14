5cb1fd4b14b3090fe918480f2b04a0beef16223f
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-09-14 07:11:06 -0700

    added new answers

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: main function description
- *
- * Return: Always 0 (Success)
- *
-*/
-
-void print_alphabet_x10(void);
-
-int main(void)
-{
-	print_alphabet_x10();
-
-	return (0);
-
-}
+#include "main.h"
 
 /**
  * print_alphabet_x10 - prints letters a - z 10x
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - check if value entered is lower character
+ * @c: parameter to be examined
+ * Description: nested while loop
+ *
+ * Return: 0 or 1 (success)
+ *
+*/
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_printchar('\n');
+}

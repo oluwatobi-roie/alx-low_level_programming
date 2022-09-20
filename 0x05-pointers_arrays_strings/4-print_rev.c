f85ed316fed486f14c2a0914cca7945cc7b29e33
Oluwatobi Victor <68193330+oluwatobi-roie@users.noreply.github.com>
2022-09-20 13:42:19 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,17 +3,22 @@
 /**
  * print_rev - Prints a string in reverse.
  * @s: The string to be printed.
+ * return: returns nothing asper void
  */
+
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i = 0;
 
-	while (s[index++])
-		len++;
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	while (*(s + i))
+	{
+		i++;
+	}
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
 	_putchar('\n');
 }
-

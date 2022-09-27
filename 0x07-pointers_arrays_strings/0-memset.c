738615916b86ab95801c43374abd824220f000c5
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-09-27 14:55:01 -0700

    fix bugs

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -23,5 +23,5 @@ char *_memset(char *s, char b, unsigned int n)
 	for (i = 0; i < n; i++)
 		memoryArea[i] = replacement;
 
-	return (memoryArea);   
+	return (memoryArea);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,24 +2,27 @@
 
 /**
  * _memcpy - entry method for the function
- * 
- * Description: The _memcpy() function copies n bytes 
+ *
+ * description: The _memcpy() function copies n bytes
  *              from memory area src to memory area dest
+ *
+ *
  * @dest: Destination of the memory tocopy
  * @src: Source of the memory we are copying
  * @n: location address of the memory we are copying
- * 
- * Returns:  a pointer to dest
+ *
+ * Return:  a pointer to dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
-    char *source = src;
-    char *destination= dest;
-    for (i = 0; i < n; i++)
-        destination[i] = source[i];
-    
-    return(destination);
+	unsigned int i;
+	char *source = src;
+	char *destination = dest;
+
+	for (i = 0; i < n; i++)
+		destination[i] = source[i];
+
+	return (destination);
 
 }

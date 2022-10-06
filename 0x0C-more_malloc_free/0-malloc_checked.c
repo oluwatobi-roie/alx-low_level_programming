79ea2a1de561ee93f3138d469c34760ae40ce709
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-06 15:45:23 -0700

    here you go

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+/**
+ * malloc_checked - function that allocate memory using malloc
+ *
+ * @b: size of the memory to be allocated.
+ *
+ * Return: Pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *allocate;
+
+	allocate = malloc(b);
+	if (allocate == NULL)
+		exit(98);
+	return (allocate);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Read me file addded for mallo free

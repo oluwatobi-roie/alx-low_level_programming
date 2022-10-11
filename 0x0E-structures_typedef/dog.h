1cc6ce0c0a30a6c4c702b66e25ebad00b05961ba
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-11 16:05:45 -0700

    last commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,8 +2,9 @@
 #define DOG_H
 
 /**
- *  * dog_t - Typedef for struct dog
- *   */
+ * dog_t - Typedef for struct dog
+ */
+
 typedef struct dog dog_t;
 
 /**
@@ -26,5 +27,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+
 
 #endif

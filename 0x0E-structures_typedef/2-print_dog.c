c64321ddf015d6fc2fd35cd6890f09e1ad9287fa
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-11 16:18:03 -0700

    hot bug dfixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,9 +18,6 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	printf("Name: %s\n", d->name);
-
-	if (!d->age)
-		printf("Age: (nil)\n");
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
--- 0x0E-structures_typedef/olddog.h
+++ 0x0E-structures_typedef/olddog.h
@@ -7,11 +7,14 @@
 typedef struct dog dog_t;
 
 /**
- * struct dog - structure for dogs
- * @name: first member
- * @age: second member
- * @owner: third member
+ * struct dog - defines the type of dog
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: who owns the dog as also a string
+ *
  */
+
 struct dog
 {
 	char *name;
@@ -21,8 +24,8 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+struct dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
 
+#endif

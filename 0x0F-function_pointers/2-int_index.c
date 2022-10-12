ff23910a541db1d91af26f28bea2ddaeda68dd45
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-12 15:55:35 -0700

    fix all betty issues

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,27 +1,26 @@
 #include "function_pointers.h"
 
 /**
- * array_interator - function that searches for an integer
- * 
+ * int_index - function that searches for an integer
+ *
  * @array: the array to be printed
  * @size: the size of the array
  * @cmp: the function pointer
- * 
+ *
  * Return: Returns nothing.
 */
 int int_index(int *array, int size, int (*cmp)(int))
-{ 
-    int i;
-    if (array == NULL || cmp == NULL)
-        return(-1);
+{
+	int i;
 
+	if (array == NULL || cmp == NULL)
+		return (-1);
 
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) != 0)
-            return(i);
-    }
-
-    return(-1);
+	return (-1);
 }

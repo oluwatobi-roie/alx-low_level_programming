dcd052dae33846d4697154c6d9b701939ebf7415
Oluwatobi Akomolafe <oluwatobi@engineertobi.com>
2022-10-12 18:10:36 +0100

    here is the header, readme and 0th task

---
+++ 0x0F_function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - functions that prints a name
+ * 
+ * @name: parameter name to be printed
+ * @f: a pointer ro a function that prints a name
+ * 
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL || f == NULL)
+        return;
+        
+    f(name);
+}
---
+++ 0x0F_function_pointers/README.md
@@ -0,0 +1,3 @@
+This is a read me file for function pointers tutorial
+
+BY: Tobi Roie

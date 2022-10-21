7229ec2630ce2d32433ac379e812f8186a69391b
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-21 05:24:10 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the element of a list_t structurelist
+ * @h: the list of elements
+ *
+ * Return: the total numbers of nodes in h
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_id = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		node_id++;
+		h = h->next;
+	}
+
+	return (node_id);
+}
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a reademe file for linked list tutorials

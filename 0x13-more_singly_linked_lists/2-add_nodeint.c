fa05b6e03c7f5a93b9ef4f2a492f4ce6a4d69397
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-24 16:11:52 -0700

    adding a new element to the frint of a list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node to te list
+ * @head: pointer to the pointer of the refrenced head
+ * @n: new entry of n for the list
+ *
+ * Return: returns the address of the new node added
+ *
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+

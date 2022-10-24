40bff0e4d463c2ae1235ed76ce5f838be2c9b35b
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-24 16:20:36 -0700

    fixed mixed declaration error

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,12 +11,13 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *new_node;
+
 	if (head == NULL)
 		return NULL;
 
-	listint_t *new_node;
-
 	new_node = malloc(sizeof(listint_t));
+	
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;

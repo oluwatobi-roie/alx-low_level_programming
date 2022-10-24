f922b1cb1b5a94c3bcc72cf6e5df7f5447c1ef84
Victor Oluwatobi <akomolafevicto@gmail.com>
2022-10-24 16:22:23 -0700

    betty fixed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,10 +14,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *new_node;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-	
+
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
